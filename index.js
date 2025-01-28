6d9a186b5e38576c16c3eafc8279ab2c7c132d79
manankhndlwl <162955550+manankhndlwl@users.noreply.github.com>
2025-01-29 00:30:48 +0530

    initialize pty process using sockets

---
+++ index.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const express = require("express");
+const { Server: SocketServer } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+require("dotenv").config();
+
+const pty = require("node-pty");
+
+const ptyProcess = pty.spawn("bash", [], {
+  name: "xterm-color",
+  cols: 80,
+  rows: 30,
+  cwd: process.env.INIT_CWD + "/user",
+  env: process.env,
+});
+
+const io = new SocketServer({
+  cors: "*",
+});
+
+io.attach(server);
+
+// for sending data to frontend from terminal
+ptyProcess.onData((data) => {
+  io.emit("terminal:data", data);
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket connected", socket.id);
+  // handling terminal input from frontend
+  socket.on("terminal:write", (data) => {
+    console.log("Term", data);
+    ptyProcess.write(data);
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`DOCKER container is running at port ${process.env.PORT}`);
+});
